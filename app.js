a4f65ea3fecbd9bf8745af1a6e7fb20757073db6
Jovian Wang <wg.jovian@gmail.com>
2022-12-19 11:29:38 -0800

    New design

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// Navbar
+const selector = document.getElementById('navbar-selector')
+const about = document.getElementById('about')
+const explore = document.getElementById('explore')
+
+function selectAbout() {
+	selector.style.left = '0';
+    about.style.left = '0';
+    explore.style.left = '100%';
+    // about.style.display = 'block';
+    // explore.style.display = 'none';
+}
+
+function selectExplore() {
+    selector.style.left = '90px';
+    about.style.left = '-100%';
+    explore.style.left = '0';
+    // explore.style.display = 'block';
+    // about.style.display = 'none';
+}
+
+
+// Timeline
+const timeline = document.getElementById("timeline-scroller");
+
+document.getElementById('timeline-scroller').addEventListener("wheel", function (e) {
+    console.log(e.target.classList)
+    e.preventDefault();
+    if (e.deltaY > 0) {
+        timeline.scrollLeft += 300;
+    } else {
+        timeline.scrollLeft -= 300;
+    }
+});
+
+
+
+
+
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const faders = document.querySelectorAll('.timeline__text');
